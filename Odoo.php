754ff292404f07173aee231025f75daf05367d56
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2016-05-20 20:58:29 +0200

    Update Odoo.php

--- Odoo.php
+++ Odoo.php
@@ -23,345 +23,345 @@ use Zend\XmlRpc\Client as XmlRpcClient;
  */
 class Odoo
 {
-	/**
-	 * Host to connect to
-	 *
-	 * @var string
-	 */
-	protected $host;
+    /**
+     * Host to connect to
+     *
+     * @var string
+     */
+    protected $host;
 
-	/**
-	 * Unique identifier for current user
-	 *
-	 * @var integer
-	 */
-	protected $uid;
+    /**
+     * Unique identifier for current user
+     *
+     * @var integer
+     */
+    protected $uid;
 
-	/**
-	 * Current users username
-	 *
-	 * @var string
-	 */
-	protected $user;
+    /**
+     * Current users username
+     *
+     * @var string
+     */
+    protected $user;
 
-	/**
-	 * Current database
-	 *
-	 * @var string
-	 */
-	protected $database;
+    /**
+     * Current database
+     *
+     * @var string
+     */
+    protected $database;
 
-	/**
-	 * Password for current user
-	 *
-	 * @var string
-	 */
-	protected $password;
+    /**
+     * Password for current user
+     *
+     * @var string
+     */
+    protected $password;
 
-	/**
-	 * XmlRpc Client
-	 *
-	 * @var XmlRpcClient
-	 */
-	protected $client;
+    /**
+     * XmlRpc Client
+     *
+     * @var XmlRpcClient
+     */
+    protected $client;
 
-	/**
-	 * XmlRpc endpoint
-	 *
-	 * @var string
-	 */
-	protected $path;
+    /**
+     * XmlRpc endpoint
+     *
+     * @var string
+     */
+    protected $path;
 
-	/**
-	 * Odoo constructor
-	 *
-	 * @param string 		   $host        The url
-	 * @param string 		   $database    The database to log into
-	 * @param string 		   $user        The username
-	 * @param string 		   $password    Password of the user
-	 * @param Zend\Http\Client $http_client An optional custom http client to initialise the XmlRpcClient with
-	 */
-	public function __construct($host, $database, $user, $password, $http_client = null)
-	{
-		$this->host = $host;
-		$this->database = $database;
-		$this->user = $user;
-		$this->password = $password;
-		$this->http_client = $http_client;
-	}
+    /**
+     * Odoo constructor
+     *
+     * @param string           $host        The url
+     * @param string           $database    The database to log into
+     * @param string           $user        The username
+     * @param string           $password    Password of the user
+     * @param array            $httpOptions Options to configure the HTTP Client of the XmlRpcClient
+     */
+    public function __construct($host, $database, $user, $password, $httpOptions = null)
+    {
+        $this->host = $host;
+        $this->database = $database;
+        $this->user = $user;
+        $this->password = $password;
+        $this->httpOptions = $httpOptions;
+    }
 
-	/**
-	 * Get version
-	 *
-	 * @return array Odoo version
-	 */
-	public function version()
-	{
-		$response = $this->getClient('common')->call('version');
+    /**
+     * Get version
+     *
+     * @return array Odoo version
+     */
+    public function version()
+    {
+        $response = $this->getClient('common')->call('version');
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * Get timezone
-	 *
-	 * @return string Current timezone
-	 */
-	public function timezone()
-	{
-		$params = array(
-			$this->database,
-			$this->user,
-			$this->password
-		);
+    /**
+     * Get timezone
+     *
+     * @return string Current timezone
+     */
+    public function timezone()
+    {
+        $params = array(
+            $this->database,
+            $this->user,
+            $this->password
+        );
 
-		return $this->getClient('common')->call('timezone_get', $params);
-	}
+        return $this->getClient('common')->call('timezone_get', $params);
+    }
 
-	/**
-	 * Search models
-	 *
-	 * @param string  $model  Model
-	 * @param array   $data   Array of criteria
-	 * @param integer $offset Offset
-	 * @param integer $limit  Max results
-	 *
-	 * @return array Array of model id's
-	 */
-	public function search($model, $data, $offset = 0, $limit = 100)
-	{
-		$params = $this->buildParams(array(
-			$model,
-			'search',
-			$data,
-			$offset,
-			$limit
-		));
+    /**
+     * Search models
+     *
+     * @param string  $model  Model
+     * @param array   $data   Array of criteria
+     * @param integer $offset Offset
+     * @param integer $limit  Max results
+     *
+     * @return array Array of model id's
+     */
+    public function search($model, $data, $offset = 0, $limit = 100)
+    {
+        $params = $this->buildParams(array(
+            $model,
+            'search',
+            $data,
+            $offset,
+            $limit
+        ));
 
-		$response = $this->getClient('object')->call('execute', $params);
+        $response = $this->getClient('object')->call('execute', $params);
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * Create model
-	 *
-	 * @param string $model Model
-	 * @param array  $data  Array of fields with data (format: ['field' => 'value'])
-	 *
-	 * @return integer Created model id
-	 */
-	public function create($model, $data)
-	{
-		$params = $this->buildParams(array(
-			$model,
-			'create',
-			$data
-		));
+    /**
+     * Create model
+     *
+     * @param string $model Model
+     * @param array  $data  Array of fields with data (format: ['field' => 'value'])
+     *
+     * @return integer Created model id
+     */
+    public function create($model, $data)
+    {
+        $params = $this->buildParams(array(
+            $model,
+            'create',
+            $data
+        ));
 
-		$response = $this->getClient('object')->call('execute', $params);
+        $response = $this->getClient('object')->call('execute', $params);
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * Read model(s)
-	 *
-	 * @param string $model  Model
-	 * @param array  $ids    Array of model id's
-	 * @param array  $fields Index array of fields to fetch, an empty array fetches all fields
-	 *
-	 * @return array An array of models
-	 */
-	public function read($model, $ids, $fields = array())
-	{
-		$params = $this->buildParams(array(
-			$model,
-			'read',
-			$ids,
-			$fields
-		));
+    /**
+     * Read model(s)
+     *
+     * @param string $model  Model
+     * @param array  $ids    Array of model id's
+     * @param array  $fields Index array of fields to fetch, an empty array fetches all fields
+     *
+     * @return array An array of models
+     */
+    public function read($model, $ids, $fields = array())
+    {
+        $params = $this->buildParams(array(
+            $model,
+            'read',
+            $ids,
+            $fields
+        ));
 
-		$response = $this->getClient('object')->call('execute', $params);
+        $response = $this->getClient('object')->call('execute', $params);
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * Update model(s)
-	 *
-	 * @param string $model  Model
-	 * @param array  $ids    Array of model id's
-	 * @param array  $fields A associative array (format: ['field' => 'value'])
-	 *
-	 * @return array
-	 */
-	public function write($model, $ids, $fields)
-	{
-		$params = $this->buildParams(array(
-			$model,
-			'write',
-			$ids,
-			$fields
-		));
+    /**
+     * Update model(s)
+     *
+     * @param string $model  Model
+     * @param array  $ids    Array of model id's
+     * @param array  $fields A associative array (format: ['field' => 'value'])
+     *
+     * @return array
+     */
+    public function write($model, $ids, $fields)
+    {
+        $params = $this->buildParams(array(
+            $model,
+            'write',
+            $ids,
+            $fields
+        ));
 
-		$response = $this->getClient('object')->call('execute', $params);
+        $response = $this->getClient('object')->call('execute', $params);
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * Unlink model(s)
-	 *
-	 * @param string $model Model
-	 * @param array  $ids   Array of model id's
-	 *
-	 * @return boolean True is successful
-	 */
-	public function unlink($model, $ids)
-	{
-		$params = $this->buildParams(array(
-			$model,
-			'unlink',
-			$ids
-		));
+    /**
+     * Unlink model(s)
+     *
+     * @param string $model Model
+     * @param array  $ids   Array of model id's
+     *
+     * @return boolean True is successful
+     */
+    public function unlink($model, $ids)
+    {
+        $params = $this->buildParams(array(
+            $model,
+            'unlink',
+            $ids
+        ));
 
-		return $this->getClient('object')->call('execute', $params);
-	}
+        return $this->getClient('object')->call('execute', $params);
+    }
 
-	/**
-	 * Get report for model
-	 *
-	 * @param string $model Model
-	 * @param array  $ids   Array of id's, for this method it should typically be an array with one id
-	 * @param string $type  Report type
-	 *
-	 * @return mixed A report file
-	 */
-	public function getReport($model, $ids, $type = 'qweb-pdf')
-	{
-		$params = $this->buildParams(array(
-			$model,
-			$ids,
-			array(
-				'model' => $model,
-				'id' => $ids[0],
-				'report_type' => $type
-			)
-		));
+    /**
+     * Get report for model
+     *
+     * @param string $model Model
+     * @param array  $ids   Array of id's, for this method it should typically be an array with one id
+     * @param string $type  Report type
+     *
+     * @return mixed A report file
+     */
+    public function getReport($model, $ids, $type = 'qweb-pdf')
+    {
+        $params = $this->buildParams(array(
+            $model,
+            $ids,
+            array(
+                'model' => $model,
+                'id' => $ids[0],
+                'report_type' => $type
+            )
+        ));
 
-		$client = $this->getClient('report');
+        $client = $this->getClient('report');
 
-		$reportId = $client->call('report', $params);
+        $reportId = $client->call('report', $params);
 
-		$state = false;
+        $state = false;
 
-		while (!$state) {
-			$report = $client->call(
-				'report_get',
-				$this->buildParams(array($reportId))
-			);
+        while (!$state) {
+            $report = $client->call(
+                'report_get',
+                $this->buildParams(array($reportId))
+            );
 
-			$state = $report['state'];
+            $state = $report['state'];
 
-			if (!$state) {
-				sleep(1);
-			}
-		}
+            if (!$state) {
+                sleep(1);
+            }
+        }
 
-		return base64_decode($report['result']);
-	}
+        return base64_decode($report['result']);
+    }
 
-	/**
-	 * Return last request
-	 *
-	 * @return string
-	 */
-	public function getLastRequest()
-	{
-		return $this->getClient()->getLastRequest();
-	}
+    /**
+     * Return last request
+     *
+     * @return string
+     */
+    public function getLastRequest()
+    {
+        return $this->getClient()->getLastRequest();
+    }
 
-	/**
-	 * Return last response
-	 *
-	 * @return string
-	 */
-	public function getLastResponse()
-	{
-		return $this->getClient()->getLastResponse();
-	}
+    /**
+     * Return last response
+     *
+     * @return string
+     */
+    public function getLastResponse()
+    {
+        return $this->getClient()->getLastResponse();
+    }
 
-	/**
-	 * Build parameters
-	 *
-	 * @param array  $params Array of params to append to the basic params
-	 *
-	 * @return array
-	 */
-	protected function buildParams(array $params)
-	{
-		return array_merge(array(
-			$this->database,
-			$this->uid(),
-			$this->password
-		), $params);
-	}
+    /**
+     * Build parameters
+     *
+     * @param array  $params Array of params to append to the basic params
+     *
+     * @return array
+     */
+    protected function buildParams(array $params)
+    {
+        return array_merge(array(
+            $this->database,
+            $this->uid(),
+            $this->password
+        ), $params);
+    }
 
-	/**
-	 * Get XmlRpc Client
-	 *
-	 * This method returns an XmlRpc Client for the requested endpoint.
-	 * If no endpoint is specified or if a client for the requested endpoint is
-	 * already initialized, the last used client will be returned.
-	 *
-	 * @param null|string $path The api endpoint
-	 *
-	 * @return XmlRpcClient
-	 */
-	protected function getClient($path = null)
-	{
-		if ($path === null) {
-			return $this->client;
-		}
+    /**
+     * Get XmlRpc Client
+     *
+     * This method returns an XmlRpc Client for the requested endpoint.
+     * If no endpoint is specified or if a client for the requested endpoint is
+     * already initialized, the last used client will be returned.
+     *
+     * @param null|string $path The api endpoint
+     *
+     * @return XmlRpcClient
+     */
+    protected function getClient($path = null)
+    {
+        if ($path === null) {
+            return $this->client;
+        }
 
-		if ($this->path === $path) {
-			return $this->client;
-		}
+        if ($this->path === $path) {
+            return $this->client;
+        }
 
-		$this->path = $path;
+        $this->path = $path;
 
-		if (!empty($this->http_client)) {
-			$this->client = new XmlRpcClient($this->host . '/' . $path, $this->http_client);
-		} else {
-			$this->client = new XmlRpcClient($this->host . '/' . $path);
-		}
+        $this->client = new XmlRpcClient($this->host . '/' . $path);
 
-		// The introspection done by the Zend XmlRpc client is probably specific
-		// to Zend XmlRpc servers. To prevent polution of the Odoo logs with errors
-		// resulting from this introspection calls we disable it.
-		$this->client->setSkipSystemLookup(true);
+        if (!empty($this->httpOptions)) {
+            $this->client->getHttpClient()->setOptions($this->httpOptions);
+        }
 
-		return $this->client;
-	}
+        // The introspection done by the Zend XmlRpc client is probably specific
+        // to Zend XmlRpc servers. To prevent polution of the Odoo logs with errors
+        // resulting from this introspection calls we disable it.
+        $this->client->setSkipSystemLookup(true);
 
-	/**
-	 * Get uid
-	 *
-	 * @return int $uid
-	 */
-	protected function uid()
-	{
-		if ($this->uid === null) {
-			$client = $this->getClient('common');
+        return $this->client;
+    }
 
-			$this->uid = $client->call('login', array(
-				$this->database,
-				$this->user,
-				$this->password
-			));
-		}
+    /**
+     * Get uid
+     *
+     * @return int $uid
+     */
+    protected function uid()
+    {
+        if ($this->uid === null) {
+            $client = $this->getClient('common');
 
-		return $this->uid;
-	}
+            $this->uid = $client->call('login', array(
+                $this->database,
+                $this->user,
+                $this->password
+            ));
+        }
+
+        return $this->uid;
+    }
 }
